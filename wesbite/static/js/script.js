a08080d4ea85b1f8a8a39debc434eb9dfd7d57c0
Dakati <chrisburgess@dhcp-v511-02800.resnet-1x-wireless.unc.edu>
2017-10-12 19:59:02 -0400

    Front end fixed
    fixed navigation bar.
    - now updates while scrolling
    - updates and waits for scroll to finish before changing
    
    removed login tab from top bar, was causing issues. Will be added back
    in once we figure out logging in.

--- wesbite/static/js/script.js
+++ wesbite/static/js/script.js
@@ -1,16 +1,41 @@
 $(document).ready(function () {
-    $('#navBar').find('li.highlight').on("click", function (e) {
+    $(document).on("scroll", onScroll);
+
+    $('#navBar').find('li').on("click", function (e) {
         $('a.active').removeClass('active');
         $(this).find('a').addClass('active');
     });
 
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        shouldChangeSelector = false;
+        $root.animate({scrollTop: $($.attr(this, 'href')).offset().top - 59}, 500, function () {
+            shouldChangeSelector = true;
+        });
+    });
 
 });
+
+var shouldChangeSelector = true;
 var $root = $('body, html');
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
-    $root.animate({scrollTop: $($.attr(this, 'href')).offset().top - 59}, 500);
-});
+
+function onScroll(event) {
+    if (shouldChangeSelector){
+        var scrollPos = $(document).scrollTop();
+        $('#navBar').find('li a').each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr('href'));
+            if (refElement.position().top <= scrollPos + 59 && refElement.position().top + refElement.height() > scrollPos - 59) {
+                console.log(true);
+                $('a.active').removeClass("active");
+                currentLink.addClass("active");
+            } else {
+                console.log(false);
+                currentLink.removeClass("active");
+            }
+        });
+    }
+}
 
 function onSignIn(googleUser) {
     var $intro = $('#intro');
@@ -25,6 +50,7 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
