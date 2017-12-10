221a8540a8019284833585102db5cc1e46a9c25d
Dakati <chrisburgess@dhcp-v880-12-00869.1Xwireless.unc.edu>
2017-12-10 15:47:18 -0500

    controller updates

--- comp_api/app/controllers/account_manager_controller.rb
+++ comp_api/app/controllers/account_manager_controller.rb
@@ -2,24 +2,38 @@ class AccountManagerController < ApplicationController
   def create_user
     if (params.has_key?(:username) &&
        params.has_key?(:password) &&
+       params.has_key?(:firstname) &&
+       params.has_key?(:lastname) &&
        Player.where(summonerName: params[:username]).count == 0)
       @user = Player.new
       @user.summonerName = params[:username]
       @user.password = params[:password]
+      @user.firstName = params[:firstname]
+      @user.lastName = params[:lastname]
       @user.save
       render json: @user
     else
-      render json: "Failed to create user (username taken or too few fields)"
+      render json: "Failed to create user, either exists or bad format. required format is create_user?username=RichardBaybaay&password=passgoeshere&firstname=Richard&lastname=Lang"
     end
   end
   def login
     if (params.has_key?(:username) &&
        params.has_key?(:password) &&
-       !User.where(name: params[:username]).select(:password).first.nil? &&
-       User.wxhere(name: params[:username]).select(:password).first.password == params[:password])
+       !Player.where(summonerName: params[:username]).select(:password).first.nil? &&
+       Player.where(summonerName: params[:username]).select(:password).first.password == params[:password])
       render json: "Login success"
     else
       render json: "Login failed"
+    end
+  end
+  def check_valid_name
+    if (params.has_key?(:username) &&
+      params[:username].length > 2 &&
+      params[:username].length < 17 &&
+        Player.where(summonerName: params[:username]).first.nil?)
+        render json: 0
+    else
+      render json: 1
     end
   end
 end
--- comp_api/config/routes.rb
+++ comp_api/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/create_user', to: 'account_manager#create_user'
   get '/login', to: 'account_manager#login'
+  get '/check_valid_name', to: 'account_manager#check_valid_name'
 
 end
