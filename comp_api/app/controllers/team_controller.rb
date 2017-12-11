cd4ecd6763943eaf99a27cea255bd6d84fb547b3
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2017-12-11 15:43:17 -0500

    Button change and team_controller creation

--- comp_api/app/controllers/team_controller.rb
+++ comp_api/app/controllers/team_controller.rb
@@ -1,2 +1,61 @@
 class TeamController < ApplicationController
+    def create_team
+        if (params.has_key?(:team_name) &&
+            params.has_key?(:tournament_id) &&
+            params.has_key?(:seed))
+            @team = Team.new
+            @team.team_name = params[:team_name]
+            @team.tournament_id = params[:tournament_id]
+            @team.seed = params[:seed]
+            @team.save
+            render json: @team
+        else
+            render json: {status: false}
+        end
+    end
+
+    def get_list
+        if (params.has_key?(:tournament_id))
+            if (Team.where(tournament_id: params[:tournament_id]).first.nil?)
+                render json: {status: false}
+            else
+                tournament_id = params[:tournament_id]
+                command = "
+                SELECT T.seed, T.team_name
+                FROM teams T
+                WHERE T.tournament_id = #{tournament_id}
+                ORDER BY T.seed
+                "
+                result = ActiveRecord::Base.connection.execute(command)
+                
+                render json: {result: result}
+            end
+        else
+            command = "
+            SELECT T.tournament_id, T.seed, T.team_name
+            FROM teams T
+            ORDER BY T.tournament_id, T.seed
+            "
+            result = ActiveRecord::Base.connection.execute(command)
+            
+            render json: {result: result}
+        end
+    end
+
+    def add_player
+        if (params.has_key?(:tournament_id) &&
+            params.has_key?(:seed) &&
+            params.has_key?(:player_id))
+            if (Team.where(tournament_id: params[:tournament_id], seed: params[:seed]).first.nil? ||
+                Player.where(id: params[:player_id]).first.nil?)
+                render json: {status: false}
+            else
+                @registration = Players_and_team.new
+                @registration.team_id = Team.where(tournament_id: params[:tournament_id], seed: params[:seed]).select(:id).first
+                @registration.player_id = Player.where(id: params[:player_id].select(:id).first)
+                @registration.save
+                render json: @registration
+            end
+        end
+    end
 end
--- comp_api/config/routes.rb
+++ comp_api/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/create_user', to: 'account_manager#create_user'
-  get '/login', to: 'account_manager#login'
-  get '/check_valid_name', to: 'account_manager#check_valid_name'
-  get '/get_user_information', to: 'account_manager#get_user_information'
+  get '/create_user',           to: 'account_manager#create_user'
+  get '/login',                 to: 'account_manager#login'
+  get '/check_valid_name',      to: 'account_manager#check_valid_name'
+  get '/get_user_information',  to: 'account_manager#get_user_information'
 
-  get '/create_tournament', to:'tournament#create_tournament'
-  get '/list_tournament', to: 'tournament#get_list'
+  get '/create_tournament',     to: 'tournament#create_tournament'
+  get '/list_tournament',       to: 'tournament#get_list'
+
+  get '/create_team',           to: 'team#create_team'
+  get '/list_team',             to: 'team#get_list'
+  get '/recruit_player',        to: 'team#add_player'
 end
