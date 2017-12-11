64a60bd39b00318f5be6e6f4c4dd13acd1d5358c
joe plever <joplever@gmail.com>
2017-12-10 22:12:28 -0500

    login

--- Front/loginButton.js
+++ Front/loginButton.js
@@ -2,16 +2,16 @@
 
 var loginButton = function(wrapper) {
     var summoner_name_form = $("<div> \n" +
-    "<form id= \"summonerName\"> Summoner Name: <br> <input type=\"text\" name= \"Summoner Name\" class = \"req_sum_name\"><br></form>\n"+
+    "<form id= \"summonerName\" class =\"wrapper_child\"> Summoner Name: <br> <input type=\"text\" name= \"Summoner Name\" class = \"req_sum_name\"><br></form>\n"+
     "</div> ");
     var first_name_form = $("<div> \n"  +
-    "<form id=\"firstName\"> First Name: <br> <input type=\"text\" name= \"First Name\" class = \"req_fir_name\"><br></form>\n" +
+    "<form id=\"firstName\" class =\"wrapper_child\"> First Name: <br> <input type=\"text\" name= \"First Name\" class = \"req_fir_name\"><br></form>\n" +
     "</div>");
     var last_name_form = $("<div>   \n" +
-    "<form id=\"lastName\"> First Name: <br> <input type=\"text\" name= \"First Name\" class = \"req_las_name\"><br></form>\n" +
+    "<form id=\"lastName\" class =\"wrapper_child\"> First Name: <br> <input type=\"text\" name= \"First Name\" class = \"req_las_name\"><br></form>\n" +
     "</div>");
     var password_form = $("<div>   \n"  +
-    "<form id=\"password\"> Password: <br> <input type=\"text\" name= \"Password\" class = \"req_password\"><br></form> \n" +
+    "<form id=\"password\" class =\"wrapper_child\"> Password: <br> <input type=\"text\" name= \"Password\" class = \"req_password\"><br></form> \n" +
     "</div>");
     $(wrapper).append(summoner_name_form, first_name_form, last_name_form, password_form);
 
@@ -52,12 +52,20 @@ var loginButton = function(wrapper) {
         console.log(req_last_name);
         console.log(req_password);
 
+        $.ajax({
+            url: 'http://localhost:3000/create_user?username=' + req_summoner_name +'&password=' + req_password + '&firstname=' + req_first_name +'&lastname=' + req_last_name,
+            success: function(e){
+              alert("success");
+            }
+          });
+
         $(summoner_name_form).find('.req_sum_name').val("");
         $(first_name_form).find('.req_fir_name').val("");
         $(last_name_form).find('.req_las_name').val("");
         $(password_form).find('.req_password').val(""); 
-    
-}
+        
+        
+}  
 }
 
     
---
+++ Front/tournamentCreation.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    var tournamentCreationPage = new tournamentCreationButton($("#wrapper")[0]);
+});
