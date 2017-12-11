9f44edfd9fd800143ccb0b1c8ddabe97e8acc46d
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2017-12-11 11:06:23 -0500

    Added more defaults in the database for functionality

--- comp_api/app/models/game.rb
+++ comp_api/app/models/game.rb
@@ -1,5 +1,13 @@
 class Game < ApplicationRecord
+    after_initialize    :defaults
+
     ## Relationships
     belongs_to      :tournament
 
+    def defaults
+        unless persisted?
+            self.game_winner_id||=0
+            self.final_round||=false
+        end
+    end
 end
--- comp_api/app/models/tournament.rb
+++ comp_api/app/models/tournament.rb
@@ -1,8 +1,15 @@
 class Tournament < ApplicationRecord
+    after_initialize    :defaults
+
     ## Relationships
     belongs_to      :player
     has_many        :teams
     has_many        :games
     has_many        :players,        :through => :teams
 
+    def defaults
+        unless persisted?
+            self.tournament_winner_id||=0
+        end
+    end
 end
