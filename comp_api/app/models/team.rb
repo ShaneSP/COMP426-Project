c1e0e554c0f3f96574216c262dfc524344524848
RichardLang2020 <31971108+RichardLang2020@users.noreply.github.com>
2017-12-10 21:57:42 -0500

    Added defaults to Teams database, briefly populated the seeds

--- comp_api/app/models/team.rb
+++ comp_api/app/models/team.rb
@@ -1,6 +1,15 @@
 class Team < ApplicationRecord
+    after_initialize    :defaults
+
     ## Relationships
     belongs_to      :tournament
     has_many        :games
     has_many        :players_and_teams
+
+    def defaults
+        unless persisted?
+            self.games_won||=0
+            self.games_played||=0
+        end
+    end
 end
