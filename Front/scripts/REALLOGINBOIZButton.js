a8dafc2f0cdd49cc9fa5d28bf250bc43ef047587
Dakati <chrisburgess@dhcp-v880-12-00869.1Xwireless.unc.edu>
2017-12-11 01:41:50 -0500

    changes

--- Front/scripts/REALLOGINBOIZButton.js
+++ Front/scripts/REALLOGINBOIZButton.js
@@ -8,7 +8,7 @@ var REALLOGINBOIZButton = function(wrapper) {
     $(wrapper).append(summoner_name_form, password_form);
 
     var login_button = document.getElementById("login_button");
-    
+
     summoner_name_form.on("submit", function(a){
         a.preventDefault();
         login_button.onclick(a);
@@ -24,7 +24,7 @@ var REALLOGINBOIZButton = function(wrapper) {
 
         var req_summoner_name = $(summoner_name_form).find('.req_sum_name').val();
         var req_password = $(password_form).find('.req_password').val();
-     
+
         if(req_summoner_name == "" ) {
             alert("You forgot your Summoner Name silly!");
             $(summoner_name_form).find('.req_sum_name').val("");
@@ -33,35 +33,38 @@ var REALLOGINBOIZButton = function(wrapper) {
 
         if(req_password == "")    {
             alert("You forgot to input your password silly!");
-            $(password_form).find('.req_password').val("");            
+            $(password_form).find('.req_password').val("");
             return false;
         }
 
-        localStorage.setItem("username", req_summoner_name);
-        localStorage.setItem("user_profile", req_summoner_name);
-        localStorage.setItem("login_state", "");
-        localStorage.setItem("logout_state", "Log Out");
-        
+
+
         console.log(req_summoner_name);
         console.log(req_password);
-        
+
         $.ajax({
-            url: 'http://localhost:3000/create_user?username=' + req_summoner_name +'&password=' + req_password,
+            url: 'http://localhost:3000/login?username=' + req_summoner_name +'&password=' + req_password,
             success: function(e){
+              console.log(e);
               if(e.status == true)  {
               alert("login successful");
+              localStorage.setItem("username", req_summoner_name);
+              localStorage.setItem("user_profile", req_summoner_name);
+              localStorage.setItem("login_state", "");
+              localStorage.setItem("logout_state", "Log Out");
               return(req_summoner_name);
               return(req_password);
+
             } else if(e.status == false)    {
               alert("login attempt failed, invalid summoner name or password");
               return false;
             }
         }
           });
-        
-      
+
+
         $(summoner_name_form).find('.req_sum_name').val("");
         $(password_form).find('.req_password').val("");
-        
+
 }
 }
