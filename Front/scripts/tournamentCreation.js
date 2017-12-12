d0a960a8acefd293bd6b6cb4149ff06a2d9711a8
annegchao <agchao22@gmail.com>
2017-12-11 21:52:10 -0500

    tournament creation UI edits

--- Front/scripts/tournamentCreation.js
+++ Front/scripts/tournamentCreation.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
 });
 
 var tournamentCreationButton = function(wrapper) {
-    var tournament_name_form = $("<div> \n" +
-    "<form id= \"tournamentName\" class =\"wrapper_child\"> Tournament Name: <br> <input type=\"text\" name= \"Tournament Name\" class = \"req_tou_name\"><br></form>\n"+
-    "</div> ");
-    var organizer_form = $("<div> \n"  +
-    "<form id=\"organizerName\" class =\"wrapper_child\"> Organizer's Summoner Name: <br> <input type=\"text\" name= \"Organizer Name\" class = \"req_org_name\"><br></form>\n" +
-    "</div>");
+    var tournament_name_form = $("<div class=\"container\">" + "<div class=\"input-group input-group-lg\" style=\"width:700px; background-color: white;\">" +
+        "<input type=\"password\" class=\"form-control req_tou_name\" placeholder=\" Tournament Name\" aria-describedby=\"basic-addon1\" style=\"text-align: center; padding-left: 0px; padding-right: 0px; padding-top: 10px; padding-bottom: 10px;\">" +
+        "</div>" + "</div>");
+    var organizer_form = $("<div class=\"container\">" + "<div class=\"input-group input-group-lg\" style=\"width:700px; background-color: white;\">" +
+        "<input type=\"password\" class=\"form-control req_org_name\" placeholder=\" Organizer Name\" aria-describedby=\"basic-addon1\" style=\"text-align: center; padding-left: 0px; padding-right: 0px; padding-top: 10px; padding-bottom: 10px;\">" +
+        "</div>" + "</div>");
     $(wrapper).append(tournament_name_form, organizer_form);
 
     var tournament_button = document.getElementById("tournament_button");
