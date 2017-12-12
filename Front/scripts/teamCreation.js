c8b2a54ad4261b8030df255766f5ec03b4f00498
joe plever <joplever@gmail.com>
2017-12-11 19:20:13 -0500

    teamCreation select and option use

--- Front/scripts/teamCreation.js
+++ Front/scripts/teamCreation.js
@@ -1,36 +1,24 @@
-function player1Function() {
-    document.getElementById("player1").classList.toggle("show");
+
+var more_teams_button = document.getElementById("moreTeamsButton");
+var to_tournaments_button = document.getElementById("returnToTournament");
+
+function anotherTeam()  {
+
 }
+var player1form = document.getElementById("player1drop");
 
-function player2Function()  {
-    document.getElementById("player2").classList.toggle("show");
-}
-
-
-function player3Function()  {
-    document.getElementById("player3").classList.toggle("show");
-}
-
-function player4Function()  {
-    document.getElementById("player4").classList.toggle("show");
-}
-
-function player5Function()  {
-    document.getElementById("player5").classList.toggle("show");
-}
-// Close the dropdown menu if the user clicks outside of it
-
-// $("div[class*=team-]")
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+function getSelectedOption(sel) {
+    var opt;
+    for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+        opt = sel.options[i];
+        if ( opt.selected === true ) {
+            break;
+        }
     }
-  } 
+    return opt;
+}
+
+function  toTournament()   {
+    console.log(getSelectedOption(player1form));
   }
+
